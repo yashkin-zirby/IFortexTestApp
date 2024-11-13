f700820f44b3a9814b62761cb2cb8751a27748fa
Yashkin <petrovskistas01@gmail.com>
2024-11-13 21:31:57 +0300

    Completed services implementation

---
+++ TestTask/TestTask/Services/Implementations/OrderService.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class OrderService : IOrderService
+    {
+        ApplicationDbContext _context;
+        public OrderService(ApplicationDbContext dbContext)
+        {
+            _context = dbContext;
+        }
+        Task<Order> IOrderService.GetOrder()
+        {
+            return _context.Orders.Where(n => n.Quantity > 1).OrderByDescending(n => n.CreatedAt).FirstAsync();
+        }
+        Task<List<Order>> IOrderService.GetOrders()
+        {
+            return _context.Orders.Where(n => n.User.Status == Enums.UserStatus.Active).OrderBy(n => n.CreatedAt).ToListAsync();
+        }
+    }
+}
---
+++ TestTask/TestTask/Services/Implementations/UserService.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using TestTask.Data;
+using TestTask.Enums;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class UserService : IUserService
+    {
+        ApplicationDbContext _context;
+        public UserService(ApplicationDbContext dbContext) {
+            _context = dbContext;
+        }
+        public Task<User> GetUser()
+        {
+            return _context.Users.OrderByDescending(
+                    user => user.Orders
+                        .Where(order => order.CreatedAt.Year == 2003 && order.Status == OrderStatus.Delivered)
+                        .Count()
+                   ).FirstAsync();
+        }
+
+        public Task<List<User>> GetUsers()
+        {
+            return _context.Users.Where(
+                    user => user.Orders.Where(order => order.CreatedAt.Year == 2010 &&
+                                (order.Status == OrderStatus.Paid || order.Status == OrderStatus.Delivered))
+                                .Count() > 0
+                    ).ToListAsync();
+        }
+    }
+}
